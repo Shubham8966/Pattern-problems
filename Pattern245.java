ff31f4cf150dd646cd96b4a7634ddd88401ffd39
Shubham8966 <103346038+Shubham8966@users.noreply.github.com>
2023-09-10 19:53:45 +0530

    Update Pattern245.java

--- Pattern245.java
+++ Pattern245.java
@@ -1,55 +1,27 @@
 /*
-*  ****   
+*  ****
 *  *   
 *  *   
 *******
    *  *
    *  *
 ****  *
- */
-public class Pattern245 
+*/
+
+public class Pattern245
 {
 	public static void main(String args[])
 	{
 		int n=7;
 		for(int i=1;i<=n;i++)
 		{
-			if(i==1||i>=((n/2)+1))
-				System.out.print("*");
-			else
-				System.out.print(" ");
-		}
-		System.out.println();
-		
-		for(int i=1;i<=(n/2)-1;i++)
-		{
 			for(int j=1;j<=n;j++)
-				if(j==1||j==((n/2)+1))
-					System.out.print("*");
-				else
-					System.out.print(" ");
+					if(i==(n/2)+1 || (i==1 && j>=(n/2)+1) || j==(n/2)+1 || (i==n && j<=(n/2)+1) || (j==1 && i<=(n/2)+1) || (j==n && i>=(n/2)+1))
+						System.out.print("*");
+					else
+						System.out.print(" ");
 			System.out.println();
 		}
 		
-		for(int i=1;i<=n;i++)
-			System.out.print("*");
-		System.out.println();
-		
-		for(int i=1;i<=(n/2)-1;i++)
-		{
-			for(int j=1;j<=n;j++)
-				if(j==((n/2)+1)||j==n)
-					System.out.print("*");
-				else
-					System.out.print(" ");
-			System.out.println();
-		}
-		
-		for(int i=1;i<=n;i++)
-			if(i<=((n/2)+1)||i==n)
-				System.out.print("*");
-			else
-				System.out.print(" ");
-		System.out.println();
 	}
 }
